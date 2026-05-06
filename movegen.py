6a192acc26cb8199f0085d5954dd4472d99f012a
Travis Eswaran <>
2026-05-06 20:43:05 +0800

    add move gen for knight and king

---
+++ movegen.py
@@ -0,0 +1,56 @@
+from constant import *
+
+class MoveGen:
+    def __init__(self):
+        self.knight_moves = self._precompute_knights()
+        self.king_moves = self._precompute_king()
+    
+    def _precompute_knights(self):
+        #Moves the knight can take
+        table = []
+        for i in range(64):
+            knight_pos = 1 << i
+            move = 0
+            move |= (knight_pos << 17) & NOT_A_FILE
+            move |= (knight_pos << 15) & NOT_H_FILE
+            move |= (knight_pos << 10) & NOT_AB_FILE
+            move |= (knight_pos << 6)  & NOT_GH_FILE
+            move |= (knight_pos >> 17) & NOT_H_FILE
+            move |= (knight_pos >> 15) & NOT_A_FILE
+            move |= (knight_pos >> 10) & NOT_GH_FILE
+            move |= (knight_pos >> 6)  & NOT_AB_FILE
+            table.append(move)
+        return table
+    
+    def _precompute_king(self):
+        #moves the king can take
+        table = []
+        for i in range(64):
+            king_pos = 1 << i
+            move = 0
+            #King wanna move up but can't when its on row 8
+            move |= (king_pos << 8) & ~RANK_8
+            #Move down but cannot when its on row 0
+            move |= (king_pos >> 8) & ~RANK_1
+            #Move right but cannot when its column h
+            move |= (king_pos << 1) & NOT_A_FILE
+            #move left but cannot when its column a
+            move |= (king_pos >> 1) & NOT_H_FILE
+            #Move NE but cannot when its at h8
+            move |= (king_pos << 9) & NOT_A_FILE
+            #Move NW but cannot when its at a8
+            move |= (king_pos << 7) & NOT_H_FILE
+            #Move SE but cannot when its at h1
+            move |= (king_pos >> 7) & NOT_A_FILE
+            #Move SW but cannot when its at a1
+            move |= (king_pos >> 9) & NOT_H_FILE
+            table.append(move)
+        return table
+
+    def get_valid_knight_moves(self, knight_pos, friendly_pos):
+        return self.knight_moves[knight_pos] &~ friendly_pos
+    
+    def get_valid_king_moves(self, king_pos, friendly_pos):
+        return self.king_moves[king_pos] &~ friendly_pos
+
+
